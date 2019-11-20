fb37e7b55d57e3b85064c71712215d72e5d5495f
tylerlay000 <46531421+tylerlay000@users.noreply.github.com>
2019-11-20 13:47:34 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+ 
+function start(){
+ 
+ let Johny = document.getElementById("noun").value;
+   let Johny = document.getElementById("noun").value; 
+   let Papa = document.getElementById("noun").value;
+   let Eating = document.getElementById("verb").value;
+    let Lies = document.getElementById("adjective1").value;
+   let Mouth = document.getElementById("body part").value;
+    let Ha = document.getElementById("onamonapia").value;
+
+    document.getElementById("myForm").innerHTML = "<h3>The best hero is <span class='word'>" + Superhero + "</span>. " + Superhero + "'s super power is <span class='word'>" + Superpower +"</span>. " + Superhero + " is <span class='word'>" + Age + "</span> years old and <span class='word'>" + Height+ "</span> feet tall. " + Superhero + " is <span class='word'>" + Adj1 + "</span>. " + Superhero +"'s enemy is <span class='word'>" + Villain +"</span>. " + Villain + " is <span class='word'>" + Adj2 + "</span>. </h3>"  
+    }
